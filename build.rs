4006c20a22a816848e128d07f0c82c74a1150adc
Yasuhiro Yamada <yamada@gr3.ie>
2023-03-11 18:27:17 +0000

    s/onig/oniguruma/

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    // Try to find libonig using pkg-config first
-    match pkg_config::Config::new().probe("onig") {
+    // Try to find oniguruma using pkg-config first
+    match pkg_config::Config::new().probe("oniguruma") {
         Ok(library) => {
             for include_path in library.include_paths {
                 println!("cargo:include={}", include_path.to_string_lossy());
