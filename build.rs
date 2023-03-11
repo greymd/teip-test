2c379805c252d9f5224e1fd252cc12da4538594a
Yasuhiro Yamada <yamada@gr3.ie>
2023-03-11 18:06:53 +0000

    Add comments

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 fn main() {
+    // Try to find libonig using pkg-config first
     match pkg_config::Config::new().probe("onig") {
         Ok(library) => {
             for include_path in library.include_paths {
                 println!("cargo:include={}", include_path.to_string_lossy());
             }
         }
+        // If pkg-config fails, leave it to the onig build script
+        // See https://crates.io/crates/onig
         Err(_) => {}
     }
 }
